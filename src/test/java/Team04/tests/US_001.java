daee0dbc1f23d486627ce0d531a93f4910e2ce72
Unwabu <simtenpefeoglu@gmail.com>
2023-01-17 10:23:13 +0300

    homepage public update

--- src/test/java/Team04/tests/US_001.java
+++ src/test/java/Team04/tests/US_001.java
@@ -1,5 +1,6 @@
 package Team04.tests;
 
+import Team04.pages.HomePage;
 import Team04.utilities.Driver;
 import Team04.utilities.ReusableMethods;
 import Team04.utilities.TestBaseReports;
@@ -7,6 +8,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class US_001 extends TestBaseReports {
+
 
 
 
