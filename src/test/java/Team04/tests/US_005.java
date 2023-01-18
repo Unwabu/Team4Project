cac96948b72c66fb18bc6f608dc0826361ac142a
F4007 - Kerim Cary <kerimcary0@gmail.com>
2023-01-18 22:37:16 +0300

    US_005 & US_009 add

---
+++ src/test/java/Team04/tests/US_005.java
@@ -0,0 +1,15 @@
+package Team04.tests;
+
+import Team04.utilities.TestBaseReports;
+import org.testng.annotations.Test;
+
+public class US_005 extends TestBaseReports {
+
+    @Test
+    public void test(){
+
+
+
+    }
+
+}
